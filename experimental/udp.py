5f17e03625a31e407e4b05ac86ac5fba04bf8232
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-06-11 06:54:28 +0200

    Update udp.py

--- experimental/udp.py
+++ experimental/udp.py
@@ -4,14 +4,12 @@ import socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific address and port
-# udp_socket.bind(('192.168.1.111', 5800))
+udp_socket.bind(('0.0.0.0', 8500))
 
 # Listen for incoming UDP packets
 while True:
-  data, addr = udp_socket.recvfrom(5800)
-  print(f'Received data from {addr}: {data.decode()}')
+  data, addr = udp_socket.recvfrom(65535)  # Use the maximum UDP packet size
+  print(f'Received data from {addr}: {data}')
 
 # Close the socket
 udp_socket.close()
-
-
