fcbd743d2aaaac009578157f97b8688a2822a579
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-06-11 06:50:47 +0200

    Update udp.py

--- experimental/udp.py
+++ experimental/udp.py
@@ -4,11 +4,11 @@ import socket
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to a specific address and port
-udp_socket.bind(('192.168.1.111', 5800))
+udp_socket.bind(('localhost', 5800))
 
 # Listen for incoming UDP packets
 while True:
-  data, addr = udp_socket.recvfrom(1024)
+  data, addr = udp_socket.recvfrom(5800)
   print(f'Received data from {addr}: {data.decode()}')
 
 # Close the socket
