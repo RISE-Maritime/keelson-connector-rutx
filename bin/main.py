023ae14475824e3d208e6487bd1268c919fe84a5
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2024-06-15 10:26:24 +0200

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -73,7 +73,7 @@ if __name__ == "__main__":
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         # Bind the socket to a specific address and port
-        udp_socket.bind(('0.0.0.0', args.upd_port))
+        udp_socket.bind(('0.0.0.0', args.udp_port))
 
         # Listen for incoming UDP packets
         while True:
