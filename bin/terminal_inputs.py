cec8280226cb52aed3f2a904f68ef1243792e170
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2024-06-15 10:31:59 +0200

    Update terminal_inputs.py

--- bin/terminal_inputs.py
+++ bin/terminal_inputs.py
@@ -47,7 +47,7 @@ def terminal_inputs():
 
     parser.add_argument(
         "--publish",
-        choices=["raw", "raw_str"],
+        choices=["raw", "raw_string"],
         type=str,
         required=False,
         action="append",
