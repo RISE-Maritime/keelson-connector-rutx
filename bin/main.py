d6fc4ccbcb7d8b8e8aa079236a476354e23f534b
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2024-06-18 22:24:25 +0200

    pub parsed nmea

--- bin/main.py
+++ bin/main.py
@@ -10,6 +10,9 @@ import socket
 from keelson.payloads.TimestampedBytes_pb2 import TimestampedBytes
 from keelson.payloads.TimestampedString_pb2 import TimestampedString
 from keelson.payloads.Log_pb2 import Log
+from keelson.payloads.NMEA_pb2 import NMEA
+import pynmea2
+
 
 session = None
 args = None
@@ -83,6 +86,21 @@ if __name__ == "__main__":
     )
     logging.info(f"Created publisher: {key_exp_pub_log}")
 
+    # NMEA GNGNS publisher 
+    key_exp_pub_nmea_gngns = keelson.construct_pub_sub_key(
+        realm=args.realm,
+        entity_id=args.entity_id,
+        subject="nmea_gngns",  # Needs to be a supported subject
+        source_id=args.source_id,
+    )
+    pub_nmea_gngns = session.declare_publisher(
+        key_exp_pub_nmea_gngns,
+        priority=zenoh.Priority.INTERACTIVE_HIGH(),
+        congestion_control=zenoh.CongestionControl.DROP(),
+    )
+    logging.info(f"Created publisher: {key_exp_pub_nmea_gngns}")
+
+
     try:
 
         # Create a UDP socket
@@ -131,6 +149,47 @@ if __name__ == "__main__":
                 envelope = keelson.enclose(serialized_payload)
                 pub_log.put(envelope)
                 logging.debug(f"...published on {key_exp_pub_log}")
+            
+            if "nmea" in args.publish:
+                logging.debug("Parsing NMEA message...")
+                # Parsing NMEA data
+                try:
+                    nmea_sentence = data.decode("utf-8")
+                    if nmea_sentence.split(',')[0] != "$GNGNS":
+                        nmea_data = pynmea2.parse(nmea_sentence)
+                        payload = NMEA()
+                        payload.timestamp.FromNanoseconds(ingress_timestamp)
+                        payload.utc.FromDatetime(nmea_data.timestamp)
+                        # Latitude 
+                        if nmea_sentence.lat_dir == "S":
+                            payload.latitude = float(-nmea_data.lat)
+                        else:
+                            payload.latitude = float(nmea_data.lat)
+                        # Longitude 
+                        if nmea_sentence.lon_dir == "W":
+                            payload.longitude = float(-nmea_data.lon)
+                        else:
+                            payload.longitude = float(nmea_data.longitude)
+
+                        payload.mode_indicator = str(nmea_data.mode_indicator)
+                        payload.satellites_used = int(nmea_data.num_sats)
+                        payload.hdop = float(nmea_data.hdop)
+                        payload.altitude = float(nmea_data.altitude)
+                        payload.geoid_height = float(nmea_data.geo_sep)
+                        serialized_payload = payload.SerializeToString()
+                        envelope = keelson.enclose(serialized_payload)
+                        pub_log.put(envelope)
+                        logging.debug(f"...published on {key_exp_pub_nmea_gngns}")
+
+
+
+                except Exception as e:
+                    logging.error(f"Error parsing NMEA data: {e}")
+                    payload.value = data.decode("utf-8")
+                nmea_data = data.decode("utf-8")
+
+
+                payload.utc = "2"
 
    
         # Close the socket
